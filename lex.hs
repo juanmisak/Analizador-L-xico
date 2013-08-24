426463869e08f0b69082c6eba71ce6cc7420541b
Juan Miteclear <juanmisak@hotmail.com>
2013-08-24 13:09:22 -0500

    add keyword tuplas to tokens_final_list

--- lex.hs
+++ lex.hs
@@ -1,8 +1,11 @@
 import Data.Char
+import Data.List
 
-let tokens_list['auto','break','case','char','const','continue','default','do','double','else','enum','extern','float','for','goto','if',
-    'int','long','register','return','short','signed','sizeof','static','struct','switch','typedef','union','unsigned','void','volatile','while']
-    
+
+let tokens_list = ["auto","break","case","char","const","continue","default","do","double","else","enum",
+		  "extern","float","for","goto","if","int","long","register","return","short","signed",
+		  "sizeof","static","struct","switch","typedef","union","unsigned","void","volatile","while"]
+      
     
 lex_alpha :: [String] -> [String]
 lex_alpha all@( alpha:[] )  = all
@@ -37,8 +40,8 @@ lex_string ( string:(c1:c2:xs):[] ) = if string == "" && c1 == '"'
 lex_char :: [String] -> [String]
 lex_char ( "":('\'':c:'\'':xs):[] ) = [ '\'':c:'\'':[], xs ]
 lex_char s = error "Wrong character literal format"
-
--- Get the next space chunck
+    
+-- Get the next space chunck 	
 lex_space :: [String] -> [String]
 lex_space all@( space:[] ) = all
 lex_space ( space:code:[] ) = if isSpace (head code)
@@ -54,5 +57,9 @@ lexemes all@(c:xs)
     | isNumber c = ( ( lex_integer ("":all:[]) ) !! 0 ) : lexemes ( ( lex_integer ("":all:[]) ) !! 1 )
     | isSpace c  = lexemes  ( lex_space("":all:[]) !! 1 )
     | otherwise  = ( ( lex_symbol ("":all:[]) ) !! 0 ) : lexemes ( ( lex_symbol ("":all:[]) ) !! 1 )
-
+    
+-- Is a keyword? yes, push at tokens_list / no, do nothing
 tokens :: [String]->[(String,String)]
+	  if any (==head lexemes) tokens_list
+	  then let tokens_final_list = tokens_list_temp ++ (head,keyword)
+	  else tokens  tail lexemes
